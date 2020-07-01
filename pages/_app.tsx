90807753d761706c4fb0f91b5722a0b5e9702f38
Admin <241538nxdd@gmail.com>
2020-07-01 18:21:51 +0700

    13 Custom App Component và Typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,52 @@
+import React from "react";
 import Head from "next/head";
+import { AppProps } from "next/app";
 import "../assets/style.scss";
 
 
-export default function MyApp({ Component, pageProps}) {
 
-    return  (
-        <div className="root-app">
-            <Head>
-                <title>Create Next App Edit</title>
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="stylesheet" href="/css/global.css" />
-            </Head>
-            <Component {...pageProps} />
-        </div>
-    )
+// export default class CustomApp extends React.Component<AppProps> {
+
+//     render(){
+//         return (
+//             <div className="root-app">
+//                 <Head>
+//                     <title>Create Next App Edit</title>
+//                     <link rel="icon" href="/favicon.ico" />
+//                     <link rel="stylesheet" href="/css/global.css" />
+//                 </Head>
+//                 <this.props.Component {...this.props.pageProps} />
+//             </div>
+//         )
+//     }
+// }
+
+const CustomAppFC: React.FC<AppProps> = ({ Component, pageProps }) => {
+        return  (
+            <div className="root-app">
+                <Head>
+                    <title>Create Next App Edit</title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="stylesheet" href="/css/global.css" />
+                </Head>
+                <Component {...pageProps} />
+            </div>
+        )
 }
+
+export default CustomAppFC;
+
+
+// export default function MyApp({ Component, pageProps}) {
+
+//     return  (
+//         <div className="root-app">
+//             <Head>
+//                 <title>Create Next App Edit</title>
+//                 <link rel="icon" href="/favicon.ico" />
+//                 <link rel="stylesheet" href="/css/global.css" />
+//             </Head>
+//             <Component {...pageProps} />
+//         </div>
+//     )
+// }
