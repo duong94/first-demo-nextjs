32b2abbb924e4eb8ff2dfb62e821ae05a9cd1437
Admin <241538nxdd@gmail.com>
2020-07-02 19:55:19 +0700

    17 Tim hieu ve useEffect trong Hooks - phan 2

--- pages/playground/hooks/lifecycle.tsx
+++ pages/playground/hooks/lifecycle.tsx
@@ -8,15 +8,32 @@ const LifeCycleDemo = () => {
     
     const [counter, setCounter] = useState(0);
     const [visible, setVisible] = useState(true);
+    const [user, setUser] = useEffect({});
+
+    // useEffect(() => {
+    //     // DidMount & DidUpdate
+    //     console.log("useEffect run");
+
+    //     return () => {
+    //         // componentWillUnmount <=> Component sẽ bij remove khỏi cấu trúc DOM
+    //     }
+    // });
+    useEffect(() => {
+        console.log("Lang nghe su thay doi cua hai state counter va visible");
+    }, [ counter, visible ])
+    useEffect(() => {
+        // DidMount - chạy một lần duy nhất sau khi render la dau tien
+        console.log("useEffect run - empty deps");
+    }, [])
 
     useEffect(() => {
-        // DidMount & DidUpdate
-        console.log("useEffect run");
+        console.log("Lang nghe su thay doi cua counter");
+    }, [counter])
 
-        return () => {
-            // componentWillUnmount <=> Component sẽ bij remove khỏi cấu trúc DOM
-        }
-    });
+    useEffect(() => {
+        console.log("Lang nghe su thay doi cua visibsle");
+    }, [visible])
+
 
     console.log("Trước khi return <=> Render");
     return (
